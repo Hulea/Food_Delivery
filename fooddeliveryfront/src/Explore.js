f00d6dda790968149863e6b5901aebb247597a4d
Hulea Andrei-Florin <61416683+Hulea@users.noreply.github.com>
2021-05-28 00:11:33 +0300

    removed google api key

--- fooddeliveryfront/src/Explore.js
+++ fooddeliveryfront/src/Explore.js
@@ -92,5 +92,5 @@ export class Explore extends React.Component{
 
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyAjybZD_vHies_ixhp9k38sK75sE17mcT8'
+    apiKey: 'inser api key here'
   })(Explore);
